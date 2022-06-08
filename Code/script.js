df442ce7b952325e8f04f67ffd52900a1093903a
Tanmay Porwal <tanmayporwal111@gmail.com>
2022-06-08 17:14:55 +0530

    .

--- Code/script.js
+++ Code/script.js
@@ -5,7 +5,6 @@ let IELTS = document.getElementById("ielts");
 let TOEIC = document.getElementById("toeic");
 let allData_1 = {};
 let allData_2 = {};
-let allData_3 = {};
 function read(){
     fetch("./UniqueIBT.json")
       .then(response => {
@@ -13,25 +12,19 @@ function read(){
     })
     .then(jsondata => allData_1 = jsondata
     );
-    fetch("./UniqueCBT.json")
+    fetch("./UniqueIELTS.json")
       .then(response => {
     return response.json();
     })
     .then(jsondata => allData_2 = jsondata
     );
-    fetch("./UniqueIELTS.json")
-      .then(response => {
-    return response.json();
-    })
-    .then(jsondata => allData_3 = jsondata
-    );
 }
 read();
 
 function convert_1(){
-    console.log(IBT.value);
+    // console.log(IBT.value);
     let output = convertion(IBT.value);
-    console.log(output);
+    // console.log(output);
     // disabled();
     
 if(IBT.value!=''){
@@ -52,7 +45,7 @@ else{
 }
 
 function convertion(IBT){
-    console.log(typeof(IBT));
+    // console.log(typeof(IBT));
     let PBTvalue;
     let CBTvalue;
     let IELTSvalue;
@@ -78,60 +71,11 @@ function convertion(IBT){
     return{PBTvalue : PBTvalue,CBTvalue : CBTvalue,IELTSvalue : IELTSvalue,TOEICvalue : TOEICvalue}
 }
 
+
 function convert_2(){
-    console.log(CBT.value);
-    let output = convertion_2(CBT.value);
-    console.log(output);
-    // disabled();
-    
-if(CBT.value!=''){
-//  console.log(output1);
-IBT.value = output.IBTvalue;
-//  console.log(minTvalue.value);
- PBT.value = output.PBTvalue;
- IELTS.value = output.IELTSvalue;
- TOEIC.value = output.TOEICvalue;
-}
-else{
-    console.log('HHHHHHHH');
-    IBT.value = '';
-    PBT.value = '';
-    IELTS.value = '';
-    TOEIC.value = '';
-}
-}
-function convertion_2(CBT){
-    console.log(typeof(CBT));
-    let IBTvalue;
-    let PBTvalue;
-    let IELTSvalue;
-    let TOEICvalue;
-
-    // console.log(allData_1[IBT]['TOEFL PBT']);
-    if(CBT == ''){
-        IBTvalue = '';
-        PBTvalue = '';
-        IELTSvalue = '';
-        TOEICvalue = '';
-    }
-
-    else if(CBT>=3 && CBT<=300){
-        IBTvalue=allData_2[CBT]['TOEFL PBT'];
-        PBTvalue=allData_2[CBT]['TOEFL CBT'];
-        IELTSvalue=allData_2[CBT]['IELTS'];
-        TOEICvalue=allData_2[CBT]['TOEIC'];
-    }
-    else {
-        return{IBTvalue : undefined, PBTvalue : undefined,IELTSvalue : undefined,TOEICvalue : undefined}
-    }
-    
-    return{IBTvalue : IBTvalue,PBTvalue : PBTvalue,IELTSvalue : IELTSvalue,TOEICvalue : TOEICvalue}
-}
-
-function convert_4(){
-    console.log(IELTS.value);
-    let output = convertion_3(IELTS.value);
-    console.log(output);
+    // console.log(IELTS.value);
+    let output = convertion_2(IELTS.value);
+    // console.log(output);
     // disabled();
    
     
@@ -152,13 +96,13 @@ else{
 }
 }
 
-function convertion_3(IELTS){
+function convertion_2(IELTS){
     console.log(typeof(IELTS));
     let IBTvalue;
     let PBTvalue;
     let CBTvalue;
     let TOEICvalue;
-    // console.log(allData_1[IBT]['TOEFL PBT']);
+    
     if(IELTS % 1 == 0 || IELTS % 1 == 0.0 ||IELTS % 1 == 0.5){
         if(IELTS == ''){
             IBTvalue = '';
@@ -168,21 +112,20 @@ function convertion_3(IELTS){
         }
 
         else if(IELTS>=0 && IELTS<=9){
-            IBTvalue=allData_3[IELTS]['TOEFL IBT'];
-            PBTvalue=allData_3[IELTS]['TOEFL PBT'];
-            CBTvalue=allData_3[IELTS]['TOEFL CBT'];
-            TOEICvalue=allData_3[IELTS]['TOEIC'];
+            IBTvalue=allData_2[IELTS]['TOEFL IBT'];
+            PBTvalue=allData_2[IELTS]['TOEFL PBT'];
+            CBTvalue=allData_2[IELTS]['TOEFL CBT'];
+            TOEICvalue=allData_2[IELTS]['TOEIC'];
         }
         else {
-            return{IBTvalue : 'Enter Valid IELTS Score[0-9]', PBTvalue : 'Enter Valid IELTS Score[0-9]',CBTvalue : 'Enter Valid IELTS Score[0-9]',TOEICvalue : 'Enter Valid IELTS Score[0-9]'}
-        }
+            return{IBTvalue : 'Enter Valid IELTS Score[0-9] with step size of 0.5 ', PBTvalue : 'Enter Valid IELTS Score[0-9] with step size of 0.5 ',CBTvalue : 'Enter Valid IELTS Score[0-9] with step size of 0.5 ',TOEICvalue : 'Enter Valid IELTS Score[0-9] with step size of 0.5 '}
+        } 
     
     }
 
 
-
     else {
-        return{IBTvalue : 'Enter Valid IELTS Score[0-9]', PBTvalue : 'Enter Valid IELTS Score[0-9]',CBTvalue : 'Enter Valid IELTS Score[0-9]',TOEICvalue : 'Enter Valid IELTS Score[0-9]'}
+        return{IBTvalue : 'Enter Valid IELTS Score[0-9] with step size of 0.5 ', PBTvalue : 'Enter Valid IELTS Score[0-9] with step size of 0.5 ',CBTvalue : 'Enter Valid IELTS Score[0-9] with step size of 0.5 ',TOEICvalue : 'Enter Valid IELTS Score[0-9] with step size of 0.5 '}
     }
     
     return{IBTvalue : IBTvalue,PBTvalue : PBTvalue,CBTvalue : CBTvalue,TOEICvalue : TOEICvalue}
